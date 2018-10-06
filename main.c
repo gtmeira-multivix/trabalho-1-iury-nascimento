cbe5a7474f0a5244f41f4f4c55e87236e542d75e
FabricioGalvani <38355745+FabricioGalvani@users.noreply.github.com>
2018-10-06 10:56:44 -0300

    implmentação codigo

--- main.c
+++ main.c
@@ -8,12 +8,47 @@ int main() {
 	//Substitua esta função pela implementação do seu trabalho.
 	printf("Digite uma expressão:\n");
 	Token t = token_proximo();
+	Pilha *pilha_s=pilha_criar();
+	Fila *fila_s=fila_criar();
+	Token Topo;
 	
-	while(t.tipo != FIM && t.tipo != ERRO) {
-		printf("\nToken = ");
-		token_imprimir(t);
+	while(t.tipo != FIM && t.tipo != ERRO){
+		//printf("\nToken = ");
+		//token_imprimir(t);
+		if(t.tipo==NUMERO){
+			fila_adicionar(fila_s,t);
+		}else if(t.tipo==OPERADOR){
+			while((pilha_primeiro(pilha_s).precedencia > t.precedencia ) ||
+				(pilha_primeiro(pilha_s).precedencia == t.precedencia && t.associatividade==ESQUERDA)){
+			
+				Topo=pilha_pop(pilha_s);
+				fila_adicionar(fila_s,Topo);
+			}
+			pilha_push(pilha_s,t);	
+		}else if(t.tipo==ABRE_PARENTESES){
+			pilha_push(pilha_s,t);
+		}else if(t.tipo==FECHA_PARENTESES){
+			while(pilha_primeiro(pilha_s) !=ABRE_PARENTESES ){
+				
+				Topo=pilha_pop(pilha_s);
+				fila_adicionar(fila_s,Topo);
+				
+			}
+			
+		}
+		
+		
 		t = token_proximo();
 	}
+	
+	while(pilha_vazia(pilha_s)!=1){
+		
+		Topo=pilha_pop(pilha_s);
+		fila_adicionar(fila_s,Topo);
+		
+		
+	}
+	
 	
 	return 0;
 }
